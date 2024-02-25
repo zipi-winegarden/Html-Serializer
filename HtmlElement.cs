bbdaff9ffcec783b8f802340c661846625f61a1e
zipi-winegarden <zipi0556776267@gmail.com>
2024-02-25 23:01:12 +0200

    commit

--- HtmlElement.cs
+++ HtmlElement.cs
@@ -45,97 +45,59 @@ public class HtmlElement
         }
     }
 
-    //public HashSet<HtmlElement> FindElementsBySelector(string selector)
-    //{
-    //    Selector selectorObject = Selector.Parse(selector);
-    //    var result = new HashSet<HtmlElement>();
-    //    FindElementsBySelectorRecursive(this, selectorObject, result);
-    //    return result;
-    //}
-
-    //private static void FindElementsBySelectorRecursive(HtmlElement element, Selector selector, HashSet<HtmlElement> result)
-    //{
-    //    if (element == null)
-    //        return;
-    //    if (MatchesSelector(element, selector))
-    //    {
-    //        if (selector.Child.Child == null)
-    //        {
-    //            result.Add(element);
-    //            return;
-    //        }
-    //    }
-
-    //    foreach (var child in element.Descendants())
-    //    {
-    //        FindElementsBySelectorRecursive(child, selector, result);
-    //    }
-    //}
-
-    //private static bool MatchesSelector(HtmlElement element, Selector selector)
-    //{
-
-    //    if (selector.TagName != element.Name)
-    //        return false;
-    //    char[] cc = new char[] { '\\', '\"' };
-    //    string elementId = null;
-    //    if (element.Id != null)
-    //        elementId = element.Id.Split(cc).First(c => c != "");
-    //    if (elementId != null && !selector.Id.Equals(elementId))
-    //        return false;
-    //    foreach (var c in selector.Classes)
-    //    {
-    //        if (element.Classes != null)
-    //        {
-    //            for (int i = 0; i < element.Classes.Count; i++)
-    //            {
-    //                element.Classes[i] = element.Classes[i].Split(cc).First(g => g != "");
-    //            }
-    //            if (!(element.Classes.Contains(c) || element.Classes.Contains(c + "\\\"") || element.Classes.Contains("\"\\" + c)))
-    //                return false;
-    //        }
-    //    }
-
-    //    return true;
-
-    //}
-    public HashSet<HtmlElement> FindElement(Selector selector)
+    public HashSet<HtmlElement> FindElementsBySelector(string selector)
     {
+        Selector selectorObject = Selector.Parse(selector);
         var result = new HashSet<HtmlElement>();
-        foreach (var child in this.Descendants())
-            child.FindElementRec(selector, result);
+        FindElementsBySelectorRecursive(this, selectorObject, result);
         return result;
+    }
 
-    }
-    public void FindElementRec(Selector selector, HashSet<HtmlElement> result)
+    private static void FindElementsBySelectorRecursive(HtmlElement element, Selector selector, HashSet<HtmlElement> result)
     {
-        if (!this.IsEqual(selector))
+        if (element == null)
             return;
-        if (selector.Child.Child == null)
+        if (MatchesSelector(element, selector))
         {
-            result.Add(this);
-            return;
+            if (selector.Child.Child == null)
+            {
+                result.Add(element);
+                return;
+            }
         }
-        foreach (var child in this.Descendants())
-            child.FindElementRec(selector.Child, result);
+
+        foreach (var child in element.Descendants())
+        {
+            FindElementsBySelectorRecursive(child, selector, result);
+        }
     }
-    public bool IsEqual(Selector selector)
+
+    private static bool MatchesSelector(HtmlElement element, Selector selector)
     {
-        if (this.Name != selector.TagName)
+
+        if (selector.TagName != element.Name)
             return false;
-        if (this.Id != selector.Id)
+        char[] cc = new char[] { '\\', '\"' };
+        string elementId = null;
+        if (element.Id != null)
+            elementId = element.Id.Split(cc).First(c => c != "");
+        if (elementId != null && !selector.Id.Equals(elementId))
             return false;
-        if (this.Classes.Count() != selector.Classes.Count())
-            return false;
-        int i = 0;
-        while (this.Classes.ToList().Count() > i && selector.Classes.ToList().Count() > i)
+        foreach (var c in selector.Classes)
         {
-            var c1 = this.Classes.ToArray()[i];
-            var c2 = selector.Classes.ToArray()[i++];
-            if (!c1.Equals(c2))
-                return false;
+            if (element.Classes != null)
+            {
+                for (int i = 0; i < element.Classes.Count; i++)
+                {
+                    element.Classes[i] = element.Classes[i].Split(cc).First(g => g != "");
+                }
+                if (!(element.Classes.Contains(c) || element.Classes.Contains(c + "\\\"") || element.Classes.Contains("\"\\" + c)))
+                    return false;
+            }
         }
+
         return true;
+
     }
 
 }
